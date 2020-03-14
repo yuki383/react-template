f315710c8515cf165cbccdc2d69526469173bedb
yuu383 <yuukikokubo.pg@gmail.com>
2020-03-14 15:00:00 +0900

    support images

--- webpack.config.js
+++ webpack.config.js
@@ -9,15 +9,22 @@ module.exports = {
         test: /\.tsx?$/,
         use: "ts-loader",
         exclude: /node_modules/
+      },
+      {
+        test: /.(png|jpe?g|gif)$/i,
+        use: [{ loader: "file-loader", }]
       }
     ]
   },
   resolve: {
-    extensions: [".tsx", ".ts", ".js", ".json"]
+    extensions: [".tsx", ".ts", ".js", ".json"],
+    alias: {
+      images: path.resolve(__dirname, "public/images")
+    }
   },
   output: {
     filename: "bundle.js",
     path: path.resolve(__dirname, "dist")
   },
-  devtool: "eval-source-map",
+  devtool: "eval-source-map"
 };

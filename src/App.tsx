57bbf27b2cdc9c68c4b8b08e4762b384efb26d59
Yuki Kokubo <yuukikokubo.pg@gmail.com>
2020-05-25 23:49:35 +0900

    chore: eslint default code

--- src/App.tsx
+++ src/App.tsx
@@ -4,5 +4,7 @@ export const App: React.FC = () => {
   const [clicked, changeClicked] = useState(false);
   const label = clicked ? "clicked!" : "click me!";
 
-  return <button onClick={() => changeClicked(prev => !prev)}>{label}</button>;
+  return (
+    <button onClick={() => changeClicked((prev) => !prev)}>{label}</button>
+  );
 };

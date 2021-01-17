86938a30e365d4940a9ec726f14ec7a5d4afd40b
yuu383 <yuukikokubo.pg@gmail.com>
2021-01-17 12:29:12 +0900

    upgrade packages

--- .eslintrc.js
+++ .eslintrc.js
@@ -24,6 +24,8 @@ module.exports = {
   },
   plugins: ["react", "@typescript-eslint", "react-hooks"],
   rules: {
+    "react/jsx-uses-react": "off",
+    "react/react-in-jsx-scope": "off",
     "react-hooks/rules-of-hooks": "error",
     "react-hooks/exhaustive-deps": "warn",
     "prettier/prettier": [
